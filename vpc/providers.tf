24dd25f875dd239f9a613ee1d6396747d28ac162
wakametarou <wakame.programing@gmail.com>
2022-03-17 18:56:48 +0900

    仮想ネットワーク作成 #4

---
+++ vpc/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+  backend "s3" {
+    bucket = "covid-mgr-container-era-tfstate"
+    key    = "vpc/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
