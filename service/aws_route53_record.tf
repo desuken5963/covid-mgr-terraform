7516db3ce905353d9b114de9e661ad0501095602
wakametarou <wakame.programing@gmail.com>
2022-03-18 22:47:33 +0900

    ALBにドメイン使用の設定 #14

--- service/aws_route53_record.tf
+++ service/aws_route53_record.tf
@@ -1,3 +1,14 @@
+resource "aws_route53_record" "covid-mgr" {
+  zone_id = data.aws_route53_zone.covid-mgr.zone_id
+  name    = data.aws_route53_zone.covid-mgr.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.lb.dns_name
+    zone_id                = aws_lb.lb.zone_id
+    evaluate_target_health = true
+  }
+}
 resource "aws_route53_record" "covid-mgr_certificate" {
   zone_id = data.aws_route53_zone.covid-mgr.zone_id
 
